08c077eeab23f542bdc2279a8c564486c6aa5702
sarteaga <>
2017-12-18 15:35:07 -0500

    variablesRegla1 {/MortgageProject/src/main/java/mupi/com/mortgageproject/loan/solicitud.java}

--- MortgageProject/src/main/java/mupi/com/mortgageproject/loan/solicitud.java
+++ MortgageProject/src/main/java/mupi/com/mortgageproject/loan/solicitud.java
@@ -12,8 +12,22 @@ public class solicitud implements java.io.Serializable
    @org.kie.api.definition.type.Label("codigoProducto")
    private String codigoProducto;
 
-   @org.kie.api.definition.type.Label(value = "resultado")
+   @org.kie.api.definition.type.Label("resultado")
    private java.lang.String resultado;
+
+   private java.lang.String grupoTipoCredito;
+
+   private java.lang.String tipoGarantia;
+
+   private java.lang.String tipoCliente;
+
+   private java.lang.Double montoSolicitado;
+
+   private java.lang.String tipoPersona;
+
+   private java.lang.String requiereGarantias;
+
+   private java.lang.String requiereGarantes;
 
    public solicitud()
    {
@@ -39,10 +53,91 @@ public class solicitud implements java.io.Serializable
       this.resultado = resultado;
    }
 
-   public solicitud(java.lang.String codigoProducto, java.lang.String resultado)
+   public java.lang.String getGrupoTipoCredito()
+   {
+      return this.grupoTipoCredito;
+   }
+
+   public void setGrupoTipoCredito(java.lang.String grupoTipoCredito)
+   {
+      this.grupoTipoCredito = grupoTipoCredito;
+   }
+
+   public java.lang.String getTipoGarantia()
+   {
+      return this.tipoGarantia;
+   }
+
+   public void setTipoGarantia(java.lang.String tipoGarantia)
+   {
+      this.tipoGarantia = tipoGarantia;
+   }
+
+   public java.lang.String getTipoCliente()
+   {
+      return this.tipoCliente;
+   }
+
+   public void setTipoCliente(java.lang.String tipoCliente)
+   {
+      this.tipoCliente = tipoCliente;
+   }
+
+   public java.lang.Double getMontoSolicitado()
+   {
+      return this.montoSolicitado;
+   }
+
+   public void setMontoSolicitado(java.lang.Double montoSolicitado)
+   {
+      this.montoSolicitado = montoSolicitado;
+   }
+
+   public java.lang.String getTipoPersona()
+   {
+      return this.tipoPersona;
+   }
+
+   public void setTipoPersona(java.lang.String tipoPersona)
+   {
+      this.tipoPersona = tipoPersona;
+   }
+
+   public java.lang.String getRequiereGarantias()
+   {
+      return this.requiereGarantias;
+   }
+
+   public void setRequiereGarantias(java.lang.String requiereGarantias)
+   {
+      this.requiereGarantias = requiereGarantias;
+   }
+
+   public java.lang.String getRequiereGarantes()
+   {
+      return this.requiereGarantes;
+   }
+
+   public void setRequiereGarantes(java.lang.String requiereGarantes)
+   {
+      this.requiereGarantes = requiereGarantes;
+   }
+
+   public solicitud(java.lang.String codigoProducto, java.lang.String resultado,
+         java.lang.String grupoTipoCredito, java.lang.String tipoGarantia,
+         java.lang.String tipoCliente, java.lang.Double montoSolicitado,
+         java.lang.String tipoPersona, java.lang.String requiereGarantias,
+         java.lang.String requiereGarantes)
    {
       this.codigoProducto = codigoProducto;
       this.resultado = resultado;
+      this.grupoTipoCredito = grupoTipoCredito;
+      this.tipoGarantia = tipoGarantia;
+      this.tipoCliente = tipoCliente;
+      this.montoSolicitado = montoSolicitado;
+      this.tipoPersona = tipoPersona;
+      this.requiereGarantias = requiereGarantias;
+      this.requiereGarantes = requiereGarantes;
    }
 
 }
