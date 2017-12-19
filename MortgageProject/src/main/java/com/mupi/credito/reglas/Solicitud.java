2ba3d5f072300d45b1ba90849f3c1a7acf8f2321
sarteaga <>
2017-12-19 15:19:48 -0500

    regla 3 4 5  {/MortgageProject/src/main/java/com/mupi/credito/reglas/Solicitud.java}

--- MortgageProject/src/main/java/com/mupi/credito/reglas/Solicitud.java
+++ MortgageProject/src/main/java/com/mupi/credito/reglas/Solicitud.java
@@ -32,6 +32,24 @@ public class Solicitud implements java.io.Serializable
 
    private java.lang.Double riesgoTotal;
 
+   private java.lang.String agencia;
+
+   private java.lang.Integer ratificada;
+
+   private java.lang.String consultaDatosSolicitud;
+
+   private java.lang.Integer codigoOficialCuenta;
+
+   private java.lang.String nombreProducto;
+
+   private java.lang.Integer nivelActual;
+
+   private java.lang.Integer ordenComite;
+
+   private java.lang.String ratificadaExt;
+
+   private java.lang.Integer solicitudRatificadaAprobada;
+
    public Solicitud()
    {
    }
@@ -146,12 +164,109 @@ public class Solicitud implements java.io.Serializable
       this.riesgoTotal = riesgoTotal;
    }
 
+   public java.lang.String getAgencia()
+   {
+      return this.agencia;
+   }
+
+   public void setAgencia(java.lang.String agencia)
+   {
+      this.agencia = agencia;
+   }
+
+   public java.lang.Integer getRatificada()
+   {
+      return this.ratificada;
+   }
+
+   public void setRatificada(java.lang.Integer ratificada)
+   {
+      this.ratificada = ratificada;
+   }
+
+   public java.lang.String getConsultaDatosSolicitud()
+   {
+      return this.consultaDatosSolicitud;
+   }
+
+   public void setConsultaDatosSolicitud(java.lang.String consultaDatosSolicitud)
+   {
+      this.consultaDatosSolicitud = consultaDatosSolicitud;
+   }
+
+   public java.lang.Integer getCodigoOficialCuenta()
+   {
+      return this.codigoOficialCuenta;
+   }
+
+   public void setCodigoOficialCuenta(java.lang.Integer codigoOficialCuenta)
+   {
+      this.codigoOficialCuenta = codigoOficialCuenta;
+   }
+
+   public java.lang.String getNombreProducto()
+   {
+      return this.nombreProducto;
+   }
+
+   public void setNombreProducto(java.lang.String nombreProducto)
+   {
+      this.nombreProducto = nombreProducto;
+   }
+
+   public java.lang.Integer getNivelActual()
+   {
+      return this.nivelActual;
+   }
+
+   public void setNivelActual(java.lang.Integer nivelActual)
+   {
+      this.nivelActual = nivelActual;
+   }
+
+   public java.lang.Integer getOrdenComite()
+   {
+      return this.ordenComite;
+   }
+
+   public void setOrdenComite(java.lang.Integer ordenComite)
+   {
+      this.ordenComite = ordenComite;
+   }
+
+   public java.lang.String getRatificadaExt()
+   {
+      return this.ratificadaExt;
+   }
+
+   public void setRatificadaExt(java.lang.String ratificadaExt)
+   {
+      this.ratificadaExt = ratificadaExt;
+   }
+
+   public java.lang.Integer getSolicitudRatificadaAprobada()
+   {
+      return this.solicitudRatificadaAprobada;
+   }
+
+   public void setSolicitudRatificadaAprobada(
+         java.lang.Integer solicitudRatificadaAprobada)
+   {
+      this.solicitudRatificadaAprobada = solicitudRatificadaAprobada;
+   }
+
    public Solicitud(java.lang.Integer codigoProducto,
          java.lang.String grupoTipoCredito, java.lang.String tipoGarantia,
          java.lang.String tipoCliente, java.lang.Double montoSolicitado,
          java.lang.String tipoPersona, java.lang.String requiereGarantias,
          java.lang.String requiereGarantes, java.lang.String resultadoScore,
-         java.lang.String estadoScoreSolicitud, java.lang.Double riesgoTotal)
+         java.lang.String estadoScoreSolicitud, java.lang.Double riesgoTotal,
+         java.lang.String agencia, java.lang.Integer ratificada,
+         java.lang.String consultaDatosSolicitud,
+         java.lang.Integer codigoOficialCuenta, java.lang.String nombreProducto,
+         java.lang.Integer nivelActual, java.lang.Integer ordenComite,
+         java.lang.String ratificadaExt,
+         java.lang.Integer solicitudRatificadaAprobada)
    {
       this.codigoProducto = codigoProducto;
       this.grupoTipoCredito = grupoTipoCredito;
@@ -164,6 +279,15 @@ public class Solicitud implements java.io.Serializable
       this.resultadoScore = resultadoScore;
       this.estadoScoreSolicitud = estadoScoreSolicitud;
       this.riesgoTotal = riesgoTotal;
+      this.agencia = agencia;
+      this.ratificada = ratificada;
+      this.consultaDatosSolicitud = consultaDatosSolicitud;
+      this.codigoOficialCuenta = codigoOficialCuenta;
+      this.nombreProducto = nombreProducto;
+      this.nivelActual = nivelActual;
+      this.ordenComite = ordenComite;
+      this.ratificadaExt = ratificadaExt;
+      this.solicitudRatificadaAprobada = solicitudRatificadaAprobada;
    }
 
 }
